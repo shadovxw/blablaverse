be5d0dcc237f65651da54c57d3780ef04aeec4c8
an-odd-sheep <vrishank.w@somaiya.edu>
2025-12-27 00:42:42 +0530

    login/logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,26 +68,36 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
     const {email, password} = req.body
-    
+
     try {
-        const user = await User.findOne({email})
+        const user = await User.findOne({email});
 
         if (!user) {
-            return res.status(400).json({message:"Invalid credentials"})
+            return res.status(400).json({message:"Invalid credentials"});
         }
 
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
         if(!isPasswordCorrect){
-            return res.status(400).json({message:"Invalid credentials"})
+            return res.status(400).json({message:"Invalid credentials"});
         }
 
         generateToken(user._id, res)
+
+        res.status(200).json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                proficPic: user.profilePic
+            });
+
     } catch (error) {
-        
+        console.log("Error in login controller: ", error);
+        return res.status(400).json({message:"Internal server error"});
     }
 }
 
-export const logout = async (req, res) => {
-
+export const logout = (_, res) => {
+    res.cookie("jwt", "", {maxAge:0})
+    res.status(200).json({message:"Logged out successfully"})
 }
